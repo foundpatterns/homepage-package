a6c523db8bcde5bf1fe74e9e74b2f8033265bd80
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2018-11-23 18:47:24 -0400

    Include slides in template

--- actions/send_homepage.lua
+++ actions/send_homepage.lua
@@ -2,10 +2,25 @@ event: ["homepage_requested"]
 priority: 1
 input_parameters: ["request"]
 
+local slideshow_id = "56d2cd3e-5357-455d-9cd4-94aa0191db8e"
+local slideshow_fields = content.read_document(slideshow_id)
+
+local slides = {}
+content.walk_documents("+model:slide", function (slide_id, fields, body)
+  if fields.slideshow == slideshow_id then
+    table.insert(slides, render("homepage-slide.html", {
+      img = fields["image-address"],
+      title = fields.title,
+      body = body,
+    }))
+  end
+end)
+
 local homepage = render("index.html", {
   SITE_URL = "/",
   SITENAME = "Lighttouch",
-  articles = {}
+  articles = {},
+  slides = slides,
 })
 
 return {
