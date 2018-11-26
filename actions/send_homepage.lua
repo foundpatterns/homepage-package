f5972955672c9e4466a78bacc9376b1a4b457dd9
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2018-11-26 15:45:01 -0400

    Sort slides by weight

--- actions/send_homepage.lua
+++ actions/send_homepage.lua
@@ -5,16 +5,23 @@ input_parameters: ["request"]
 local slideshow_id = torchbear.settings.slideshow
 local slideshow_fields = content.read_document(slideshow_id)
 
-local slides = {}
+local slide_weights = {}
+
 content.walk_documents(nil, function (slide_id, fields, body)
   if fields.model == "slide" and fields.slideshow == slideshow_id then
-    table.insert(slides, render("homepage-slide.html", {
-      img = fields["image-address"],
-      title = fields.title,
-      body = body,
-    }))
+    table.insert(slide_weights, {
+      content = render("homepage-slide.html", {
+        img = fields["image-address"],
+        title = fields.title,
+        body = body,
+      }),
+      weight = fields.weight or math.maxinteger,
+    })
   end
 end)
+
+table.sort(slide_weights, function (a, b) return a.weight < b.weight end)
+local slides = map(slide_weights, function (it) return it.content end)
 
 local homepage = render("index.html", {
   SITE_URL = "/",
